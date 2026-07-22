dc7e00119fe58fa7589ab40b42b4b3a2b3e42511
Dev Patmase <devpatmase1@gmail.com>
2026-07-23 03:18:36 +0530

    Added gunicorn for deployment

--- backend/services/vector_service.py
+++ backend/services/vector_service.py
@@ -3,10 +3,12 @@ import numpy as np
 import pickle
 import os
 
-VECTOR_DIR = "vector_store"
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(BASE_DIR)
+
+VECTOR_DIR = os.path.join(PROJECT_ROOT, "vector_store")
 
 os.makedirs(VECTOR_DIR, exist_ok=True)
-
 
 def save_embeddings(embeddings, chunks):
 
